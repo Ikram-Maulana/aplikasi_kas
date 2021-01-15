351a52ff696958f70b8fb7aeaae67f7393cd1451
Ikram-Maulana <ikram_maulana@onedrive.web.id>
2021-01-15 23:24:35 +0700

    dashboard

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -9,12 +9,62 @@
      </div>
 
      <div class="section-body">
+       <div class="row">
+         <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+           <div class="card card-statistic-1">
+             <div class="card-icon bg-primary">
+               <i class="fas fa-donate"></i>
+             </div>
+             <div class="card-wrap">
+               <div class="card-header">
+                 <h4>Total Dana Masuk</h4>
+               </div>
+               <div class="card-body">
+                 Rp
+                 <?= number_format($total_masuk['nominal']); ?>
+               </div>
+             </div>
+           </div>
+         </div>
+         <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+           <div class="card card-statistic-1">
+             <div class="card-icon bg-danger">
+               <i class="fas fa-shopping-bag"></i>
+             </div>
+             <div class="card-wrap">
+               <div class="card-header">
+                 <h4>Total Dana Keluar</h4>
+               </div>
+               <div class="card-body">
+                 Rp
+                 <?= number_format($total_kas2['nominal']); ?>
+               </div>
+             </div>
+           </div>
+         </div>
+         <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+           <div class="card card-statistic-1">
+             <div class="card-icon bg-warning">
+               <i class="fas fa-file-alt"></i>
+             </div>
+             <div class="card-wrap">
+               <div class="card-header">
+                 <h4>Total Saldo</h4>
+               </div>
+               <div class="card-body">
+                 Rp
+                 <?= number_format($total_masuk['nominal'] - $total_kas2['nominal']); ?>
+               </div>
+             </div>
+           </div>
+         </div>
+       </div>
        <div class="col-12 mb-4">
          <div class="hero text-white hero-bg-image hero-bg-parallax"
            data-background="<?= base_url('assets'); ?>/img/unsplash/andre-benz-1214056-unsplash.jpg">
            <div class="hero-inner">
              <h2>Welcome Back, <?= $user['name']; ?>!</h2>
-             <p class="lead">This page is a place to see dana masuk, dana keluar and saldo saat ini.
+             <p class="lead">Halaman ini adalah tempat untuk melihat dana masuk, dana keluar dan saldo saat ini.
              </p>
            </div>
          </div>
